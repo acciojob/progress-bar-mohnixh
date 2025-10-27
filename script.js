46009852c2150d24993c0e7e4f68c355628f04ca
mohnixh <153740400+mohnixh@users.noreply.github.com>
2025-10-27 11:51:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-//your JS code here. If required.
+const nextBtn = document.querySelector('#next');
+const prevBtn = document.querySelector('#prev');
+const circles = document.querySelectorAll('.circle-child');
+
+let activeIndex = 0;
+
+nextBtn.addEventListener('click', function () {
+    console.log('nxtClicked');
+    
+    if (activeIndex < circles.length) {
+        circles[activeIndex].style.backgroundColor = 'black';
+        prevBtn.disabled = false
+        prevBtn.style.backgroundColor = ' rgba(186, 25, 25, 1)'
+
+        if (activeIndex === circles.length - 1) {
+            nextBtn.disabled = true;
+            nextBtn.style.backgroundColor = 'rgba(186, 25, 25, 1)'
+        }
+        activeIndex++;
+    }
+});
+
+prevBtn.addEventListener('click', function () {
+    console.log('prvClicked');
+
+    if (activeIndex > 0) {
+        activeIndex--;
+        circles[activeIndex].style.backgroundColor = 'gray';
+
+        if (activeIndex < circles.length) {
+            nextBtn.style.backgroundColor = '';
+            nextBtn.disabled = false;
+        }
+
+        if(activeIndex === 0){
+            prevBtn.disabled = true;
+            prevBtn.style.backgroundColor = 'gray'
+        }
+
+    }
+});
